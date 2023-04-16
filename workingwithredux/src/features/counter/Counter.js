0370ebb5a69c8ddc6d2a737fc1f34efa192ad743
prabhanjansp <puranikchetan97@gmail.com>
2023-04-16 14:51:33 +0530

    Intro to Redux State Management

---
+++ workingwithredux/src/features/counter/Counter.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement, reset, incrementByAmount } from "./counterSlice";
+import { useState } from "react";
+
+const Counter = () => {
+  const [incrementAmount, setIncrementAmount] = useState(0);
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+  const addValue = Number(incrementAmount) || 0;
+  const resetAll = () => {
+    setIncrementAmount(0);
+    dispatch(reset());
+  };
+
+  return (
+    <section>
+      <p>{count}</p>
+      <div className="btn">
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(decrement())}>-</button>
+      </div>
+      <input
+        type="text"
+        value={incrementAmount}
+        onChange={(e) => setIncrementAmount(e.target.value)}
+      />
+      <div>
+        <button onClick={() => dispatch(incrementByAmount(addValue))}>
+          INCREMENT BY AMOUNT
+        </button>
+
+        <button onClick={resetAll}>RESET</button>
+      </div>
+    </section>
+  );
+};
+
+export default Counter;
